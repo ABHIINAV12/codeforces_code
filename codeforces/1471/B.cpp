7559dc580c287b644c1b6ffd35cf2312dd627ef1
Abhinav Batta <iit2018010@iiita.ac.in>
2021-01-05 20:49:00 +0530

    Add solution for problem `B - Strange List` Link: https://codeforces.com/contest/1471/problem/B Tags: brute force, greedy, implementation, math, *1100 Ref: https://codeforces.com/contest/1471/submission/103423400

---
+++ codeforces/1471/B.cpp
@@ -0,0 +1,77 @@
+//  You're not going to master the rest of your life in one day.
+//  Just relax. Master the day. 
+//  Then just keep doing that every day.
+#include<bits/stdc++.h> 
+using namespace std;
+#define int long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define f(i,x,n) for(int i=x;i<(int)n;++i)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+#define pq priority_queue<int>
+#define pqs priority_queue<int,vi,greater<int>>
+#define vpii vector<pair<int,int>>
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end()
+#define sz(x) x.size()
+#define mpi map<int,int>
+
+int mod=1e9+7;
+
+int lcm(int a,int b){
+	int g=__gcd(a,b);
+	a*=b; a/=g;
+	return a;
+}
+
+void solve(){
+	int n,x; cin>>n>>x;
+	int a[n]; f(i,0,n) cin>>a[i];
+	int s=0; int b[n]; f(i,0,n){
+		s+=a[i];
+		int cnt=0;
+		int c=a[i]; while(c%x==0){
+			++cnt;
+			c/=x;
+		}
+		b[i]=cnt;
+	}
+	int mn=LLONG_MAX; f(i,0,n) mn=min(mn,b[i]);
+	bool pp=0; f(i,0,n){
+		if(b[i]==mn) pp=1;
+		if(pp==1) b[i]=mn;
+		else b[i]=mn+1;
+	}
+	f(i,0,n) s+=b[i]*a[i];
+	cout<<s<<"\n";
+}
+
+
+void input(){
+	int t=1; cin>>t;
+	f(T,1,t+1)
+		solve(); 
+}
+ 
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);	
+	#ifdef Me
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);	
+	#endif
+	srand(time(0));
+	input();
+	return 0;
+}
+
+// ============== Notes =============== //
+// common bugs
+// - fastio
+// - int overflow (especially intermediate expressionns)
+// - array bounds (indices bigger than MAXN?)
+// - special cases (n=1? graph not connected?)
+// - re-initialize efficiently between test cases
