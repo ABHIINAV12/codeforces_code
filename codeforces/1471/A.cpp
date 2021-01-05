6c47666bdd6808410b5ffb8f9108de3c68ebb872
Abhinav Batta <iit2018010@iiita.ac.in>
2021-01-05 20:08:00 +0530

    Add solution for problem `A - Strange Partition` Link: https://codeforces.com/contest/1471/problem/A Tags: greedy, math, number theory, *900 Ref: https://codeforces.com/contest/1471/submission/103391912

---
+++ codeforces/1471/A.cpp
@@ -0,0 +1,66 @@
+//  You're not going to master the rest of your life in one day.
+//  Just relax. Master the day. 
+//  Then just keep doing that every day.
+#include<bits/stdc++.h> 
+using namespace std;
+#define int long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define f(i,x,n) for(int i=x;i<(int)n;++i)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+#define pq priority_queue<int>
+#define pqs priority_queue<int,vi,greater<int>>
+#define vpii vector<pair<int,int>>
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end()
+#define sz(x) x.size()
+#define mpi map<int,int>
+
+int mod=1e9+7;
+
+int lcm(int a,int b){
+	int g=__gcd(a,b);
+	a*=b; a/=g;
+	return a;
+}
+
+void solve(){
+	int n,x; cin>>n>>x;
+	int s=0; int mn=0; 
+	f(i,0,n){
+		int xx; cin>>xx;
+		s+=xx; mn+=(xx+x-1)/x;
+	}
+	s=(s+x-1)/x;
+	cout<<s<<" "<<mn<<"\n";
+}
+
+
+void input(){
+	int t=1; cin>>t;
+	f(T,1,t+1)
+		solve(); 
+}
+ 
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);	
+	#ifdef Me
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);	
+	#endif
+	srand(time(0));
+	input();
+	return 0;
+}
+
+// ============== Notes =============== //
+// common bugs
+// - fastio
+// - int overflow (especially intermediate expressionns)
+// - array bounds (indices bigger than MAXN?)
+// - special cases (n=1? graph not connected?)
+// - re-initialize efficiently between test cases
