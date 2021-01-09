25385b52fed990e851e58aaf548adc555d0cb300
Abhinav Batta <iit2018010@iiita.ac.in>
2021-01-09 12:25:00 +0530

    Add solution for problem `A - Wizard of Orz` Link: https://codeforces.com/contest/1467/problem/A Tags: constructive algorithms, greedy, math, *900 Ref: https://codeforces.com/contest/1467/submission/103853500

---
+++ codeforces/1467/A.cpp
@@ -0,0 +1,81 @@
+//Abhinav ---IIIT_A
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define f(i,x,n) for(int i=x;i<(int)n;++i)
+#define vpii vector<pair<int,int>> 
+#define vi vector<int>
+#define vpii vector<pair<int,int>> 
+#define mpii map<pair<int,int>,int>
+#define mpivpii map<int,vector<pair<int,int>>> 
+#define pii pair<int,int> 
+#define all(x) x.begin(),x.end()
+#define sz(x) x.size()
+#define mpi map<int,int>
+#define vvi vector<vector<int>>
+
+ld pie=3.141592653589793238462643383279;
+int mod=1e9+7;
+
+vi vt;
+
+void st(int id,int curr,int l,int r){
+  if(l==r){
+    if(id==l)vt[curr]=1;
+    return ;
+  }
+  int mid=(l+r)/2;
+  if(id>=l && id<=mid) st(id,2*curr+1,l,mid);
+  else st(id,2*curr+2,mid+1,r);
+  vt[curr]=vt[2*curr+1]+vt[2*curr+2];
+}
+
+
+int gt(int curr,int l,int r,int lx){
+  if(r<lx) return 0;
+  if(l==r){
+    if(l==lx) return vt[curr];
+    return 0;
+  }
+  int mid=(l+r)/2;
+  // no , par, com
+  if(l>=lx) return vt[curr];
+  else return gt(2*curr+1,l,mid,lx) + gt(2*curr+2,mid+1,r,lx);
+}
+
+int a[75][75];
+int dp[75][75][75][75];
+
+int32_t main(){ 
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+ 	int t; cin>>t;
+ 	while(t--){
+ 		int n; cin>>n;
+ 		f(i,0,n) {
+ 			if(i==0) {cout<<"9"; continue;}
+ 			else{
+ 				cout<<(i+7)%10;
+			 }
+		 }
+		 cout<<"\n";
+	 }
+  return 0; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
