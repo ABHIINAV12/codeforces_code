abd753b18fdb4d1d102356934fe3177f49100e54
Abhinav Batta <iit2018010@iiita.ac.in>
2021-01-20 20:38:00 +0530

    Add solution for problem `B - Different Divisors` Link: https://codeforces.com/contest/1474/problem/B Tags: binary search, constructive algorithms, greedy, math, number theory, *1000 Ref: https://codeforces.com/contest/1474/submission/104916378

---
+++ codeforces/1474/B.cpp
@@ -0,0 +1,83 @@
+//  You're not going to master the rest of your life in one day.
+//  Just relax. Master the day. 
+//  Then just keep doing that every day.
+#include<bits/stdc++.h> 
+using namespace std;
+#define int long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define f(i,x,n) for(int i=x;i<(int)n;++i)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+#define pq priority_queue<int>
+#define pqs priority_queue<int,vi,greater<int>>
+#define vpii vector<pair<int,int>>
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define mpi map<int,int>
+#define d1(x,y) cout<<x<<" ----------------------------- "<<y<<"\n"
+
+int mod=1e9+7;
+
+int lcm(int a,int b){
+	int g=__gcd(a,b);
+	a*=b; a/=g;
+	return a;
+}
+
+const int mxn=3010000;
+
+int rep[mxn];
+void pri(){
+	rep[0]=1;
+	rep[1]=1;
+	f(i,2,mxn){
+		if(rep[i]==0)
+		for(int j=i;j<mxn;j+=i){
+			rep[j]=i;
+		}
+	}
+}
+ 
+
+void solve(){
+	int d; cin>>d;
+	int p=-1,q=-1;
+	for(int i=1+d;;++i){
+		bool ck=1; for(int j=2;j*j<=i;++j) if(i%j==0) {ck=0; break;}
+		if(ck){p=i; break;}
+	}
+	for(int i=p+d;;++i){
+		bool ck=1; for(int j=2;j*j<=i;++j) if(i%j==0) {ck=0; break;}
+		if(ck){q=i; break;}
+	}
+	cout<<p*q<<"\n";
+}
+
+
+void input(){
+	int t=1; cin>>t;
+	f(T,1,t+1)
+		solve(); 
+}
+ 
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);	
+	#ifdef Me
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);	
+	#endif
+	srand(time(0));
+	input();
+	return 0;
+}
+
+// ============== Notes =============== //
+/*
+	
+
+*/
