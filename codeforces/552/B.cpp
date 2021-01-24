f4f0c37eb5c2e6f6e17508f6ab556db6f3c7276a
Abhinav Batta <iit2018010@iiita.ac.in>
2021-01-24 10:51:00 +0530

    Add solution for problem `B - Vanya and Books` Link: https://codeforces.com/contest/552/problem/B Tags: implementation, math, *1200 Ref: https://codeforces.com/contest/552/submission/105196063

---
+++ codeforces/552/B.cpp
@@ -0,0 +1,57 @@
+//  You're not going to master the rest of your life in one day.
+//  Just relax. Master the day. 
+//  Then just keep doing that every day.
+#include<bits/stdc++.h> 
+using namespace std;
+#define int long long
+#define ld long double
+#define pb push_back
+#define ff first
+#define ss second
+#define f(i,x,n) for(int i=x;i<(int)n;++i)
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vvvi vector<vector<vector<int>>>
+#define pq priority_queue<int>
+#define pqs priority_queue<int,vi,greater<int>>
+#define vpii vector<pair<int,int>>
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end()
+#define sz(x) (int)x.size()
+#define mpi map<int,int>
+#define d1(x,y) cout<<x<<" ----------------------------- "<<y<<"\n"
+
+int mod=1e9+7;
+
+ 
+int32_t main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);	
+	srand(time(0));
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	int t=1; //cin>>t;
+	while(t--){
+		int n; cin>>n;
+		int here=9; int ret=0,curr=1;
+		while(1){
+			if(here>n){
+				break;
+			}else{
+				ret+=here*curr; n-=here;
+				here*=10;
+				++curr;
+			}
+		}
+		ret+=n*curr;
+		cout<<ret<<"\n";
+	}
+	return 0;
+}
+
+// ============== Notes =============== //
+/*
+	
+
+*/
